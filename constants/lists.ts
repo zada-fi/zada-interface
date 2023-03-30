95ad67728ff000c1df2d37bcf6282b5eb1e618c4
zada-fi <128002274+zada-fi@users.noreply.github.com>
2023-03-30 23:48:28 +0800

    Add files via upload
    init code

---
+++ constants/lists.ts
@@ -0,0 +1,6 @@
+// the Uniswap Default token list lives here
+export const DEFAULT_TOKEN_LIST_URL = 'https://raw.githubusercontent.com/zada-finance/token-list/main/zada.tokenlist.json'
+
+export const DEFAULT_LIST_OF_LISTS: string[] = [
+  DEFAULT_TOKEN_LIST_URL
+]
